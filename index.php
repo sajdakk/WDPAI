61a7cc5bb1cd553e398a22c2f178dbfa3511976e
sajdakk <anita.sajdak123@gmail.com>
2023-11-09 20:27:22 +0100

    Fix

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ $path = $_SERVER["REQUEST_URI"];
 $path = trim($path, "/");
 $path = parse_url($path, PHP_URL_PATH);
 
-Routing::get('', 'DefaultController');
 Routing::get('login', 'DefaultController');
 Routing::get('dashboard', 'DefaultController');
 Routing::get('top', 'DefaultController');
